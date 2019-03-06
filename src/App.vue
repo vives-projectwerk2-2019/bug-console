861bd270cf171f9a992c0195b997cf2d5cf38fcc
Jop Bouckaert <jop.bouckaert@outlook.com>
2019-03-06 15:56:11 +0100

    update logo

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <img alt="BUG logo" src="./assets/logo.png" height="25%" width="25%">
     <BugConsole msg="BUG-console"/>
   </div>
 </template>
